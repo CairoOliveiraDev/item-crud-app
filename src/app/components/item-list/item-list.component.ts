90806e2637b6d0a338f6278005df1da86bea4e42
Cairo Oliveira <cairo.aco@hotmail.com>
2024-08-08 16:58:57 -0300

    some bugs to fix

--- src/app/components/item-list/item-list.component.ts
+++ src/app/components/item-list/item-list.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ItemService } from '../../services/item.service';
+import { Item } from '../../models/item';
 
 @Component({
   selector: 'app-item-list',
-  standalone: true,
-  imports: [],
   templateUrl: './item-list.component.html',
-  styleUrl: './item-list.component.css'
+  styleUrls: ['./item-list.component.css']
 })
-export class ItemListComponent {
+export class ItemListComponent implements OnInit {
+  items: Item[] = [];
 
+  constructor(private itemService: ItemService) {}
+
+  ngOnInit(): void {
+    this.itemService.getItems().subscribe((items) => {
+      this.items = items;
+    });
+  }
+
+  deleteItem(id: number): void {
+    if (confirm('Are you sure you want to delete this item?')) {
+      this.itemService.deleteItem(id).subscribe(() => {
+        this.items = this.items.filter(item => item.id !== id);
+      });
+    }
+  }
 }
